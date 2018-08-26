7074e010302f3bdd1263b221fbb6b823f1ad4d66
dlhede1509@gmail.com <dlhede1509@gmail.com>
2018-08-26 01:29:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,8 @@
 def display_rainbow(colors)
     @display_rainbow
     colors = ["red, orange, yellow, green, blue, indigo, violet"]
-    return puts "#{R: red[0]}, #{O: orange[1]}, Y: yellow, G: green, B: blue, I: indigo, V: violet" 
+    return puts "#{R: red[0]}, #{O: orange[1]}, #{Y: yellow[2]}, #{G: green[3]}, #{B: blue[4]}, #{I: indigo[5]}, 
+    #{V: violet[6]}" 
 end 
   
   
